822bef069a74004c5ff13191926befa0060d8c4d
smsievers <seth_sievers@yahoo.com>
2020-07-15 13:18:30 -0500

    before class 4

--- crowdfund.rb
+++ crowdfund.rb
@@ -10,9 +10,28 @@ class Project
         "Project #{@name} has #{@funds} dollars in funding towards its goal of #{@goal} dollars."
     end
 
-    def addFunds(number)
+    def add_funds(number)
         puts "#{number} dollars were added to project #{@name}"
         @funds += number 
     end
 
+    def withdraw_funds(number)
+        puts "#{number} dollars were removed from project #{@name}"
+        @funds -= number
+    end
+end
+
+class Category
+    def initialize(title)
+        @title = title
+        projects = []
+    end
+
+    def add_project(project)
+        projects.push(project)
+    end
+    
+    def display
+        puts "There are #{@projects.size} in the #{@title} category."
+    end
 end
